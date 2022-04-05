cab3aab4ebe31ce8e9d596ceefda4f5555cb5f2d
mbbrv <beburov1@gmail.com>
2022-04-06 01:13:03 +0300

    added command care

---
+++ cmd/care.go
@@ -0,0 +1,28 @@
+// Package cmd /*
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// careCmd represents the care command
+var careCmd = &cobra.Command{
+	Use:   "care",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("care called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(careCmd)
+
+}
